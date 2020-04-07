9d881e18e8fddc813069c84c9d21d4ad9ea84eca
wangyahong423 <2976716563@qq.com>
2020-04-07 15:25:44 +0800

    v0.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,12 +17,45 @@ module.exports = function (grunt) {
         eslintrc: '.eslintrc.json'
       },
       target: ['*.js']
-    }
+    },
+    mocha: {
+      test: {
+        src: ['test/index.html'],
+      },
+      options: {
+        run: true,
+        reporter: 'Dot'
+      }
+    },
+    htmlmin: {
+      options: {
+        collapseWhitespace: true,
+        preserveLineBreaks: false
+      },
+      files: {
+        src: './index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    cssmin: {
+      'dist/rectangle.css': 'rectangle.css'
+    },
+    uglify: {
+      release:{
+        files: {
+          'dist/rectangle.js': 'rectangle.js',
+        }
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-htmlhint');
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['htmlhint','csslint','eslint']);
+  grunt.registerTask('default', ['htmlhint','csslint','eslint','htmlmin','cssmin','uglify']);
 };
